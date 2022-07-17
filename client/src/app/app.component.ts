9afe5cfc90b8b609e82776f06f8a6ea055214c2a
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-07-17 14:45:26 +1000

    Make the HTTP request to get users

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    // httpClient is naturally asyncronous, it returns Observable, use subscribe to decide what to do afterwards
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error)
+    })
+  }
+
 }
