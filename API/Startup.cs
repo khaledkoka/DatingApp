c611bedf118e6191c9d2b65ddcd3f849f86fe727
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-07-17 14:51:53 +1000

    Add CORS support in the API

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -52,6 +53,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            // Use Cors to solve API error, must be added between UseRouting and UseEndpoints
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200")); 
 
             app.UseAuthorization();
 
