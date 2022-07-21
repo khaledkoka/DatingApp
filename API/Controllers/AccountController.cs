9add4b7f9dc75456ba893d96ab624672c3cf9ebe
Khaled <khaled.kokah@gmail.com>
2022-07-21 15:54:10 +1000

    Check if username exists

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -20,9 +21,12 @@ namespace API.Controllers
 
         [HttpPost("register")]
         public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto){
+            if(await UserExists(registerDto.Username)) return BadRequest("Username is taken.");
+
             using var hmac = new HMACSHA512();
+
             var user = new AppUser{
-                UserName= registerDto.Username,
+                UserName= registerDto.Username.ToLower(),
                 PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
@@ -31,6 +35,10 @@ namespace API.Controllers
             await _context.SaveChangesAsync();
 
             return user;
+        }
+
+        private async Task<bool> UserExists(string username){
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
