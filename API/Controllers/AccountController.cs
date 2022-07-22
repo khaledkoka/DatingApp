ff916b46f149c395d7676f4fad2f78b61797a03f
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-07-22 20:10:25 +1000

    DB changes

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -42,7 +42,7 @@ namespace API.Controllers
         public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
             var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
            
-            if(user == null)  return Unauthorized("Invalid Username.");
+            if(user == null) return Unauthorized("Invalid Username.");
             
             using var hmac = new HMACSHA512(user.PasswordSalt);
             
