97b266f116570f15811fd1b6cced478c5959ab3c
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-07-25 19:52:08 +1000

    Persisting the login [observables/localStorage]

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -13,6 +13,7 @@ export class NavComponent implements OnInit {
   constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.getCurrentUser();
   }
 
   login() {
@@ -24,8 +25,16 @@ export class NavComponent implements OnInit {
     })
   }
 
-  logout(){
+  logout() {
+    this.accountService.logout();
     this.loggedIn = false;
   }
 
+  getCurrentUser() {
+    this.accountService.currentUser$.subscribe(user => {
+      this.loggedIn = !!user;
+    }, error => {
+      console.log(error);
+    })
+  }
 }
