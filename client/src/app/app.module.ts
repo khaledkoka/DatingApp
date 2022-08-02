4e2993f9f00a0e6965e5822374c5ebbf9f9f7ce1
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-08-02 19:58:30 +1000

    Adding an error interceptor [read description]
    Why adding the error interceptor? Because we want to make sure that we do encounter any of these errors [400, 401, the ones we created], we handle them at a global level inside Angular.  And that's where we need to use HTTP interceptors.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,6 +14,7 @@ import { MessagesComponent } from './members/messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
+import { ErrorInterceptor } from './_interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,9 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
     FormsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true } // multi:true as we want to add it to the default angular interceptors
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
