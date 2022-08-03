eb2e7192653e04baaf5452aaa94e4bacefbeadd2
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-08-03 15:32:21 +1000

    Flatten the array

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -28,7 +28,7 @@ export class ErrorInterceptor implements HttpInterceptor {
                     modalStateErrors.push(error.error.errors[key]);
                   }
                 }
-                throw modalStateErrors;
+                throw modalStateErrors.flat();
               } else {
                 this.toastr.error(error.statusText === "OK" ? "Bad Request" : error.statusText, error.status);
               }
