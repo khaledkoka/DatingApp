1810581520aa7bc3c84d80f5da90b0a0abd967ba
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-08-10 20:50:18 +1000

    Create Users repository
    The repository pattern acts provides abstraction from DbContext.
    1. It Encapsulates the logic. [In DbContext for example, we have Users.First(), Users.FirstOrDefault(), Users.Includes(x=>x.Thing).FirstOrDefault, etc)
    - In a Repository, it'we can create methods like GetUser(), GetUsers, SaveAll(), etc which makes it easier and cleaner.
    
    2. It reduces duplicate query logic.
    - For example, if we have a DbContext function used in several controller, and that function consists of several LINQ queries, we can create a repository and use one method in all controllers.
    
    3. Makes it easier for unit testing.

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByUsername(string username)
+        {
+            return await _context.Users.SingleOrDefaultAsync(x => x.UserName == username);
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsersAsync()
+        {
+           return await _context.Users.ToListAsync();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            // Check if data has been saved in the database
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public void Update(AppUser user)
+        {
+            // Add a flag to the entity and set its state as modified
+            _context.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
