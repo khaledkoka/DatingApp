fa449610e60c083ce46abaabf808ab6244212af4
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-08-10 21:12:02 +1000

    Create Member and Photo DTOs
    We need the DTOs to get only specific data. Also it will help solving the circular reference issue that happened

---
+++ API/DTOs/MemberDto.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class MemberDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public int Age { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+    }
+}
