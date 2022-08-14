fd517376ecb9e169004db6ff57ca0466bb8fa205
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-08-15 09:51:45 +1000

    Use environmen.ts to replace hardcoded strings

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,13 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
+  
   // Observable so that our auth guard is able to subscribe to this [route guards automatically subscribe to observables]
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
