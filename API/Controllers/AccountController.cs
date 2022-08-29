015bbc7203b715aa9b2790082386cd1502519090
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-08-29 19:06:47 +1000

    Add the main photo image to the nav

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -46,7 +46,7 @@ namespace API.Controllers
 
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto){
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            var user = await _context.Users.Include(p => p.Photos).SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
            
             if(user == null) return Unauthorized("Invalid Username.");
             
@@ -60,7 +60,8 @@ namespace API.Controllers
 
             return new UserDto {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -10,7 +10,7 @@ import { User } from '../_models/user';
 })
 export class AccountService {
   baseUrl = environment.apiUrl;
-  
+
   // Observable so that our auth guard is able to subscribe to this [route guards automatically subscribe to observables]
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
@@ -22,25 +22,24 @@ export class AccountService {
       map((response: User) => {
         const user = response;
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
-    );
+    )
   }
 
   register(model: any) {
     return this.http.post(this.baseUrl + 'account/register', model).pipe(
       map((user: User) => {
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     )
   }
 
   setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
