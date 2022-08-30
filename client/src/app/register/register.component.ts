f9eee91d0d28f74a7f0e1078f0d26f26cc84ecbd
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-08-31 08:48:32 +1000

    Add custom client validators [match password]

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -24,10 +24,20 @@ export class RegisterComponent implements OnInit {
       username: new FormControl('Hello', Validators.required),
       password: new FormControl('', [Validators.required,
       Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('', Validators.required)
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    })
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+      this.registerForm.controls.confirmPassword.updateValueAndValidity();
     })
   }
-  
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control?.value === control?.parent?.controls[matchTo].value
+        ? null : { isMatching: true }
+    }
+  }
+
   register() {
     console.log(this.registerForm.value);
     // this.accountService.register(this.model).subscribe(response => {
