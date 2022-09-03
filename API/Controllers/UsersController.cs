680f18eee54ff317f1f8e99da25a8c52defe1d31
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-09-03 21:10:30 +1000

    Add filtering to the API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -33,6 +33,12 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery] UserParams userParams) // We need FromQuery in case of not supplying paramaters in query string in HTTPGet request 
         {
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+            userParams.CurrentUsername = user.UserName;
+
+            if (string.IsNullOrEmpty(userParams.Gender))
+                userParams.Gender = user.Gender == "male" ? "female" : "male";
+
             var users = await _userRepository.GetMembersAsync(userParams);
 
             // Use the extension method that we created for the response headers
