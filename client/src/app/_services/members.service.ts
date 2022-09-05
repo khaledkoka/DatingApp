8a1462149e5f425600e118631c92c07e12b861f0
Khaled <khaled.kokah@gmail.com>
2022-09-05 20:29:10 +1000

    Restore caching for member details

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -38,6 +38,14 @@ export class MembersService {
   }
 
   getMember(username: string) {
+    const member = [...this.memberCache.values()]
+      .reduce((arr, elem) => arr.concat(elem.result), [])
+      .find((member: Member) => member.username === username);
+
+    if (member) {
+      return of(member);
+    }
+
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
