2aa65336dfe8bc346e9d534cb3b6a85801a3a94f
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-09-06 19:54:33 +1000

    Add pagination for the likes

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -5,19 +5,8 @@ using System.Threading.Tasks;
 
 namespace API.Helpers
 {
-    public class UserParams
+    public class UserParams : PaginationParams
     {
-        // Pagination
-        private const int MaxPageSize = 50;
-        public int PageNumber { get; set; } = 1;
-        private int _pageSize = 10;
-
-        public int PageSize
-        {
-            get => _pageSize;
-            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
-        }
-
         // Filtering
         public string CurrentUsername { get; set; }
         public string Gender { get; set; }
