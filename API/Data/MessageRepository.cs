890ddf7621a2f4498f73327da901450f45e0b271
Khaled <khaled.kokah@gmail.com>
2022-09-09 09:38:34 +1000

    Get the messages from repo

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -6,14 +6,18 @@ using API.DTOs;
 using API.Entities;
 using API.Helpers;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 
 namespace API.Data
 {
     public class MessageRepository : IMessageRepository
     {
         private readonly DataContext _context;
-        public MessageRepository(DataContext context)
+        private readonly IMapper _mapper;
+        public MessageRepository(DataContext context, IMapper mapper)
         {
+            _mapper = mapper;
             _context = context;
         }
 
@@ -42,19 +46,33 @@ namespace API.Data
             throw new NotImplementedException();
         }
 
-        public async Task<bool> SaveAllAsync()
+        async Task<PagedList<MessageDto>> IMessageRepository.GetMessagesForUser(MessageParams messageParams)
         {
-            return await _context.SaveChangesAsync() > 0; // Return true or false
-        }
+            var query = _context.Messages
+                .OrderByDescending(m => m.MessageSent)
+                .AsQueryable();
 
-        Task<PagedList<MessageDto>> IMessageRepository.GetMessagesForUser()
-        {
-            throw new NotImplementedException();
+            query = messageParams.Container switch
+            {
+                "Inbox" => query.Where(u => u.Recipient.UserName == messageParams.Username),
+                "Outbox" => query.Where(u => u.Sender.UserName == messageParams.Username),
+                _ => query.Where(u => u.Recipient.UserName ==
+                    messageParams.Username && u.DateRead == null)
+            };
+
+            var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
+
+            return await PagedList<MessageDto>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
         }
 
         Task<IEnumerable<MessageDto>> IMessageRepository.GetMessageThread(int currentUserId, int recipentId)
         {
             throw new NotImplementedException();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0; // Return true or false
         }
     }
 }
