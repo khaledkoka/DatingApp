c6fd411e33f31ebcd8cb0be386dbbfda1100543b
Khaled <khaled.kokah@gmail.com>
2022-09-13 09:32:25 +1000

    Load messages when activating messages tab

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -8,19 +8,10 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit {
-  @Input() username: string;
-  messages: Message[];
+  @Input() messages: Message[];
 
-  constructor(private messageService: MessageService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.loadMessages();
   }
-
-  loadMessages() {
-    this.messageService.getMessageThread(this.username).subscribe(messages => {
-      this.messages = messages;
-    })
-  }
-
 }
