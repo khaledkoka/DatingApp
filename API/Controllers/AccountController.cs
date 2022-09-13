e3ea56f3be288615baeeae8bab69ad7002bda911
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-09-13 18:24:12 +1000

    .Net Identity: Set up entities

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -33,11 +33,7 @@ namespace API.Controllers
 
             var user = _mapper.Map<AppUser>(registerDto);
 
-            using var hmac = new HMACSHA512();
-
             user.UserName = registerDto.Username.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-            user.PasswordSalt = hmac.Key;
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
@@ -57,15 +53,6 @@ namespace API.Controllers
             var user = await _context.Users.Include(p => p.Photos).SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
             if (user == null) return Unauthorized("Invalid Username.");
-
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for (int i = 0; i < computedHash.Length; i++)
-            {
-                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password.");
-            }
 
             return new UserDto
             {
--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -18,10 +18,7 @@ namespace API.Data
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
             foreach(var user in users){
-                using var hmac = new HMACSHA512();
                 user.UserName = user.UserName.ToLower();
-                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("P@$$w0rd"));
-                user.PasswordSalt = hmac.Key;
                 
                 context.Users.Add(user);
             }
