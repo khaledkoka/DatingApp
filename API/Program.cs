b441a09fd6e4351b521a90a0d8296cac3158e046
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-09-13 18:55:36 +1000

    .Net Identity: Update the database seed method

--- API/Program.cs
+++ API/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,12 +21,15 @@ namespace API
             var host = CreateHostBuilder(args).Build();
             using var scope = host.Services.CreateScope();
             var services = scope.ServiceProvider;
-            try{
+            try
+            {
                 var context = services.GetRequiredService<DataContext>();
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
                 await context.Database.MigrateAsync();
-                // await Seed.SeedUsers(context);
+                await Seed.SeedUsers(userManager);
             }
-            catch(Exception ex){
+            catch (Exception ex)
+            {
                 var logger = services.GetRequiredService<ILogger<Program>>();
                 logger.LogError(ex, "An error occurred during migration");
             }
