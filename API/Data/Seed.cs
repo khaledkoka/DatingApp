01dea285476d30b8b775ee1c6c7d8453f2ca0c74
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-09-14 07:48:04 +1000

    .Net Identity: Add roles to the app

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -13,20 +13,43 @@ namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(UserManager<AppUser> userManager)
+        public static async Task SeedUsers(UserManager<AppUser> userManager,
+            RoleManager<AppRole> roleManager)
         {
             if (await userManager.Users.AnyAsync()) return;
 
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData
                 , new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-            
+
+            if (users == null) return;
+
+            var roles = new List<AppRole>
+            {
+                new AppRole { Name = "Member" },
+                new AppRole { Name = "Admin" },
+                new AppRole { Name = "Moderator" },
+            };
+
+            foreach (var role in roles)
+            {
+                await roleManager.CreateAsync(role);
+            }
+
             foreach (var user in users)
             {
                 user.UserName = user.UserName.ToLower();
-
                 await userManager.CreateAsync(user, "P@$$w0rd");
+                await userManager.AddToRoleAsync(user, "Member");
             }
+
+            var admin = new AppUser
+            {
+                UserName = "admin"
+            };
+
+            await userManager.CreateAsync(admin, "P@$$w0rd");
+            await userManager.AddToRolesAsync(admin, new[] { "Admin", "Moderator" });
         }
     }
 }
