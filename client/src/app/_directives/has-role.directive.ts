d83966d46ad27f8b3b888e7704ead79c0913256d
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-09-14 15:23:49 +1000

    Add a custom directive for admin menu link

---
+++ client/src/app/_directives/has-role.directive.ts
@@ -0,0 +1,36 @@
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+import { take } from 'rxjs/operators';
+
+@Directive({
+  selector: '[appHasRole]' // it'll be *appHasRole selector like *ngIf, *ngFor, bsRadio
+})
+export class HasRoleDirective implements OnInit {
+  @Input() appHasRole: string[];
+  user: User;
+
+  constructor(private viewContainerRef: ViewContainerRef,
+    private templateRef: TemplateRef<any>,
+    private accountService: AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
+      this.user = user;
+    })
+  }
+
+  ngOnInit(): void {
+    debugger;
+    // Clear the view if no roles 
+    if (!this.user?.roles || this.user == null) {
+      this.viewContainerRef.clear();
+      return;
+    }
+
+    if (this.user?.roles.some(r => this.appHasRole.includes(r))) {
+      this.viewContainerRef.createEmbeddedView(this.templateRef);
+    }
+    else {
+      this.viewContainerRef.clear();
+    }
+  }
+}
