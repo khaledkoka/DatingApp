9ec524e72924639f1ccc6f49e0462792a5062e5d
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-09-15 15:49:55 +1000

    SignalR: Add a presence tracker

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -7,6 +7,7 @@ using API.DTOs;
 using API.Helpers;
 using API.Interfaces;
 using API.Services;
+using API.SignalR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,6 +18,7 @@ namespace API.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
+            services.AddSingleton<PresenceTracker>();
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IPhotoService, PhotoService>();
--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -11,14 +11,28 @@ namespace API.SignalR
     [Authorize]
     public class PresenceHub : Hub
     {
+        private readonly PresenceTracker __tracker;
+        public PresenceHub(PresenceTracker _tracker)
+        {
+            __tracker = _tracker;
+        }
+
         public override async Task OnConnectedAsync()
         {
+            await __tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId):
             await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+
+            var currentUsers = await __tracker.GetOnlineUsers();
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
+            await __tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId):
             await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+
+            var currentUsers = await __tracker.GetOnlineUsers();
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
 
             await base.OnDisconnectedAsync(exception);
         }
