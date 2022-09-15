88779ee809d76610c6011a2c85e13cb5885467a9
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-09-15 12:52:24 +1000

    SignalR: Set up client side basics

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
+import { PresenceService } from './_services/presence.service';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,7 @@ export class AppComponent implements OnInit {
   title = 'Dating App';
   users: any;
 
-  constructor(private accountService: AccountService) {
+  constructor(private accountService: AccountService, private presence: PresenceService) {
 
   }
 
@@ -20,8 +21,11 @@ export class AppComponent implements OnInit {
     this.setCurrentUser();
   }
 
-  setCurrentUser(){
-    const user: User = JSON.parse(localStorage.getItem('user')); 
-    this.accountService.setCurrentUser(user);
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      this.accountService.setCurrentUser(user);
+      this.presence.createHubConnection(user);
+    }
   }
 }
