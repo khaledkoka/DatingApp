997653b0d2f8c115f13bc9ae3ed1615f8797b105
Khaled <32701068+spectrecal@users.noreply.github.com>
2022-09-16 10:01:50 +1000

    SignalR: Create a message hub

--- API/Startup.cs
+++ API/Startup.cs
@@ -76,6 +76,7 @@ namespace API
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
+                endpoints.MapHub<MessageHub>("hubs/message");
             });
         }
     }
